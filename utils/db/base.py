fcccc18cf3f736dfe2e1394baaa85abd7d5db48f
netbriler <netbriler@gmail.com>
2021-07-28 22:51:22 +0300

    Remove BaseModel

--- utils/db/base.py
+++ utils/db/base.py
@@ -1,18 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .models.base import BaseModel
-
 from data.config import SQLALCHEMY_DATABASE_URI
+
+Base = declarative_base()
 
 
 async def create_async_database():
     engine = create_async_engine(SQLALCHEMY_DATABASE_URI)
 
     async with engine.begin() as conn:
-        # await conn.run_sync(BaseModel.metadata.drop_all)
-        await conn.run_sync(BaseModel.metadata.create_all)
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
     async_session = sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
--- utils/db/models/user.py
+++ utils/db/models/user.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, DateTime, Integer, String
 
-from .base import BaseModel
+from ..base import Base
 
 
-class User(BaseModel):
+class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     username = Column(String, default=None)
     language = Column(String, default='en')
+
+    created_at = Column(DateTime, default=lambda: datetime.utcnow(), )
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
