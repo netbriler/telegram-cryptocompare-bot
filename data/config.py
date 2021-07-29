58f77e4a154cc136bd184b6196e70f3f98c36b3f
netbriler <netbriler@gmail.com>
2021-07-29 14:10:15 +0300

    Add postgresql

--- data/config.py
+++ data/config.py
@@ -1,13 +1,23 @@
-from decouple import config as env_conf
 import os
+
+from decouple import config as env_conf
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
 BOT_TOKEN = env_conf("BOT_TOKEN")
 ADMINS = list(map(int, env_conf("ADMINS").split(',')))
 
-SQLALCHEMY_DATABASE_URI = f'sqlite+aiosqlite:///{DIR}/database.sqlite3'
+DB_USER = env_conf('DATABASE_USER')
+DB_PASSWORD = env_conf('DATABASE_PASS')
+DB_HOST = env_conf('DATABASE_HOST')
+DB_PORT = env_conf('DATABASE_PORT')
 
+DB_NAME = env_conf('DATABASE_NAME', default='database.sqlite3', cast=str)
+
+SQLALCHEMY_DATABASE_URI = f'sqlite+aiosqlite:///{DIR}/{DB_NAME}'
+
+if DB_USER and DB_PASSWORD:
+    SQLALCHEMY_DATABASE_URI = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 I18N_DOMAIN = 'bot'
 LOCALES_DIR = f'{DIR}/locales'
