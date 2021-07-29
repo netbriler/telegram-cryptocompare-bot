b4c0023d8d5147c0241c8a1418a63eeac44e8ef4
netbriler <netbriler@gmail.com>
2021-07-29 19:05:33 +0300

    Refactoring

--- keyboards/inline/help.py
+++ keyboards/inline/help.py
@@ -6,7 +6,7 @@ from loader import _
 def get_help_inline_markup():
     markup = InlineKeyboardMarkup()
 
-    markup.add(InlineKeyboardButton(_('Цена'), switch_inline_query_current_chat=''))
+    markup.add(InlineKeyboardButton(_('Цена'), switch_inline_query_current_chat='DOGE'))
     markup.add(InlineKeyboardButton(_('Конвертировать'), switch_inline_query_current_chat='2 ETH USD,EUR'))
 
     return markup
